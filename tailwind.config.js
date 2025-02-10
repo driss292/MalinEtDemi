68a727efa30304e0d5fd33953e86c432be609bb7
driss292 <drisskaci@outlook.fr>
2025-02-10 15:58:36 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        anton: ["Anton", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
