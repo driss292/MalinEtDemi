af12b07f1f35cf4b021d456a5c820b10f5c8fd8b
driss292 <drisskaci@outlook.fr>
2025-02-10 15:17:06 +0100

    setup project

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { NgClass, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [NgClass, NgIf],
+  templateUrl: './header.component.html',
+  styles: ``,
+})
+export class HeaderComponent {
+  isMenuOpen = false;
+  logoPath = './assets/fox.png'; // Définition du chemin vers le logo
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
